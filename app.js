7325857f79891898d9c7d0eca491e2281c8cb253
かろん <104133597+karonyt@users.noreply.github.com>
2023-02-27 11:10:11 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const firebaseConfig = {
 	apiKey: "AIzaSyBjHhgwJ24qN4ZM0xGVVkG4S-pNqO_rwuM",
 	authDomain: "chat-94055.firebaseapp.com",
-	databaseURL: "https://chat-94055.firebaseio.com",
+	databaseURL: "https://chat-94055-default-rtdb.firebaseio.com/",
 	projectId: "chat-94055",
 	storageBucket: "chat-94055.appspot.com",
         messagingSenderId: "96214020304",
@@ -11,24 +11,54 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
-// Get a reference to the Firebase database
-const database = firebase.database().ref();
+// Firebase Realtime Databaseの設定
+var firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://chat-94055-default-rtdb.firebaseio.com/", // Realtime DatabaseのURL
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
 
-// Get a reference to the messages container
-const messages = document.querySelector('#messages');
+// Firebase Realtime Databaseの初期化
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
 
-// Listen for new messages added to the database
-database.child('messages').on('child_added', function(snapshot) {
-	const message = snapshot.val();
-	const messageElement = document.createElement('div');
-	messageElement.textContent = message;
-	messages.appendChild(messageElement);
-});
+// チャットメッセージを送信する関数
+function sendMessage() {
+  var name = document.getElementById("name").value;
+  var message = document.getElementById("message").value;
 
-// Handle the form submit event
-document.querySelector('form').addEventListener('submit', function(event) {
-	event.preventDefault();
-	const message = document.querySelector('#message').value;
-	database.child('messages').push(message);
-	document.querySelector('#message').value = '';
-});
+  // Realtime Databaseにメッセージを追加する
+  database.ref("messages").push({
+    name: name,
+    message: message
+  });
+
+  // メッセージ入力欄をクリアする
+  document.getElementById("message").value = "";
+}
+
+// Realtime Databaseからメッセージを取得するコールバック関数
+function onMessageAdded(snapshot) {
+  var messages = document.getElementById("messages");
+  messages.innerHTML = "";
+
+  snapshot.forEach(function(childSnapshot) {
+    var message = childSnapshot.val();
+    var name = message.name;
+    var text = message.message;
+
+    var messageElement = document.createElement("li");
+    var nameElement = document.createElement("strong");
+    nameElement.innerText = name + ": ";
+    messageElement.appendChild(nameElement);
+    messageElement.appendChild(document.createTextNode(text));
+    messages.appendChild(messageElement);
+  });
+}
+
+// Realtime Databaseの"messages"ノードに対するリスナーを登録する
+database.ref("messages").on("child_added", onMessageAdded);
